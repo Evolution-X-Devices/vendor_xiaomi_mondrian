212dc15675910bcf38aaf413d364780a1ed40380
flakeforever <113103193+flakeforever@users.noreply.github.com>
2025-04-12 01:15:07 -0700

    mondrian: Update blobs from OS2.0.5.0.VMNMIXM

--- proprietary/vendor/bin/init.kernel.post_boot.sh
+++ proprietary/vendor/bin/init.kernel.post_boot.sh
@@ -131,13 +131,8 @@ function configure_memory_parameters() {
 
 	configure_zram_parameters
 	configure_read_ahead_kb_values
-	
-        echo 100 > /proc/sys/vm/swappiness
-	if [ "$ProductName" == "yudi" ]; then
-		echo 10 > /proc/sys/vm/watermark_scale_factor
-	else
-		echo 1 > /proc/sys/vm/watermark_scale_factor
-	fi
+	echo 100 > /proc/sys/vm/swappiness
+	echo 1 > /proc/sys/vm/watermark_scale_factor
 
     echo 0 > /proc/sys/vm/watermark_boost_factor
 
